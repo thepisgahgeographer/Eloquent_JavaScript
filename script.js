16f225dd7a4eb89978b9de6553ddae1ec461df68
Yuri Potawsky <ypotawsky@gmail.com>
2019-04-04 10:52:56 -0400

    Objects

--- script.js
+++ script.js
@@ -1,114 +1,19 @@
-let test = (testing) => {
-    window.alert(testing);
-}
+//document.write("Test" + "<br><br><br>");
 
-test("hello");
+// let bike = "Salsa";
+// document.write(bike.toUpperCase());
+// document.write(typeof bike);
 
-// var test = document.getElementById("test").addEventListener("click", popUp);
+//Constructing Objects
+// let bike = {};
+// bike.color = "Red";
+// document.write(bike.color);
 
-// function popUp() {
-//     document.getElementById("two").innerHTML = "Hello World";
-//     window.alert("hello");
-// }
+let fullSuspension = {
+    bike : "Salsa Rustler",
+    color : "Blue Fade",
+    components : ["XT Brakes" , "Zee Rear Der" , "Race Face Cranks"]
+};
 
-// popUp();
-
-// function Counter(i){
-//         for(i; i<10; i++){
-//            "<p>" + document.write(i) + "</p>";
-//         };
-// }
-
-// test;
-
-// Counter(5);
-
-/*Requests input from the user and checks to see if it matches the type*/
-// let theNumber = Number(window.prompt("Number is"));
-
-// if (!Number.isNaN(theNumber)){
-//     console.log("Number Is" + theNumber);
-// }
-
-// let bike = "grey";
-// if (bike != "grey"){
-//     console.log("False");
-// }
-// else if (bike == "orange") {
-//     console.log("False2");
-// }
-// else {
-//     console.log("True");
-// }
-
-// let number = 0;
-// while(number<=12){
-//     console.log(number++);
-// }
-
-// let result = 2;
-// let counter = 0;
-
-// while (counter <10){
-//     result = result *2;
-//     counter++;
-// }
-
-// console.log(result);
-
-// for(let counter = 0; counter <=10; counter++){
-//     console.log(counter);
-//     //break;
-// }
-
-// switch(window.prompt("Bike Color")){
-//     case "red":
-//     console.log("red");
-//     case "black":
-//     console.log("black");
-//     break;
-// }
-
-// let size = 8;
-
-// let board = "";
-
-// for (let y = 0; y < size; y++) {
-//     for (let x = 0; x < size; x++) {
-//         if ((x + y) % 2 == 0) {
-//             board += " ";
-//         } else {
-//             board += "#";
-//         }
-//         console.log(y + x);
-//     }
-
-//     board += "\n";
-// }
-
-// console.log(board);
-
-
-
-// let Pisgah = ["The", " Greatest", " National Forest"];
-
-// Pisgah[3] = "red";
-// console.log(Pisgah);
-
-// let Wilsons = new Array();
-// console.log(typeof Wilsons);
-// Wilsons[0] = "Trail";
-// Wilsons[1] = "Mtn Bike";
-// console.log(Wilsons[1]);
-
-// let list = [];
-
-// let addEntry = (event, ride) => {
-//     list.push({ event, ride });
-// }
-
-// let test = addEntry(["nuts"], "no");
-// test;
-
-// let selection = document.getElementById("thisOne");
-// selection.innerHTML(list);
+//document.write(fullSuspension.color + "<br>" + fullSuspension.components);
+document.write(Object.keys(fullSuspension));
