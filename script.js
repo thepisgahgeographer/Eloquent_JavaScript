9821891726841dc96e990fecd286b1cd01ea0722
Yuri Potawsky <ypotawsky@gmail.com>
2019-04-04 16:00:35 -0400

    Updated

--- script.js
+++ script.js
@@ -9,11 +9,27 @@
 // bike.color = "Red";
 // document.write(bike.color);
 
-let fullSuspension = {
-    bike : "Salsa Rustler",
-    color : "Blue Fade",
-    components : ["XT Brakes" , "Zee Rear Der" , "Race Face Cranks"]
-};
+// let fullSuspension = {
+//     bike : "Salsa Rustler",
+//     color : "Blue Fade",
+//     components : ["XT Brakes" , "Zee Rear Der" , "Race Face Cranks"]
+// };
 
-//document.write(fullSuspension.color + "<br>" + fullSuspension.components);
-document.write(Object.keys(fullSuspension));
+// //document.write(fullSuspension.color + "<br>" + fullSuspension.components);
+// document.write(Object.keys(fullSuspension));
+
+//Create an empty array and push parameters to it from the addEntry(function)
+let journal = [];
+let addEntry = (input)=>{
+    journal.push([input]);
+}
+
+addEntry(["red", "Ragley", "Race Face", "Shimano XT"], true );
+//document.write(journal);
+
+for(let value of journal){
+    document.write(value);
+    
+}
+
+
